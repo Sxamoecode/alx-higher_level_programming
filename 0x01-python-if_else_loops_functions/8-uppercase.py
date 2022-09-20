bd29967728c2fb041e7b867dedb0f64efbfe3450
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-20 07:20:35 -0700

    Updating

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) >= 65 and ord(str) <= 90:
-        return True
-    else:
-        return False
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] <= 'z':
+            ch = str[i]
+            ch = ord(ch)
+            ch = ch - 32
+            ch = chr(ch)
+            str = str[:i] + ch + str[i+1:]
+        return str
