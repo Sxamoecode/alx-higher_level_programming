0847c19a8873ff0512aaa2105cce4130c3f9f818
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-20 06:35:35 -0700

    Updating

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digit1 in range(0, 9):
+    for digit2 in range(digit1+1, 10):
+        print(f"{digit1:d}".format(digit1), end='')
+        print(f"{digit2:d}".format(digit2), end=', ')
+        if digit1 == 8 and digit2 == 9:
+            continue
