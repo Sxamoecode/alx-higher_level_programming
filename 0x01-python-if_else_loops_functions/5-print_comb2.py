7a7fa4c5508b7c691cdf465310477c1277efed97
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-20 12:06:12 -0700

    Updating

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    num_str = str(num)
-    if len(num_str) == 1:
-        print(f"0{num:d}".format(num), end='')
-        print(", ", end='')
-        continue
-    if num == 99:
-        print(f"{num:d}".format(num), end='')
-        continue
-    print(f"{num:d}".format(num), end=', ')
+    print(f"{num:02d}".format(num), end=', ')
