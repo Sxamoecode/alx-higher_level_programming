c4baa1d7b12284470e49f5e0c901f5a899c45ba2
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-22 11:26:59 -0700

    Updating

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    Add = add(a, b)
-    print(f"{a:d} + {b:d} = {Add:d}".format(a, b, Add))
+    print(f"{a:d} + {b:d} = {add(a, b):d}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print(f"0 arguments")
+elif len(sys.argv) == 2:
+    print(f"1 argument: {sys.argv[1]}")
+else:
+    print(f"{len(sys.argv) - 1:d} arguments:".format(len(sys.argv) - 1))
+argv = len(sys.argv)
+for i in range(1, argv):
+    print(f"{i:d}: {sys.argv[i]:s}".format(i, sys.argv[i]))
