d39648e802ed0b2ecd3e09ce41a34fc18bdaac0b
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-28 10:12:48 -0700

    Updating

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    for i in range(0, len(my_list)):
+        if my_list[i] not in my_list[:i]:
+            result += my_list[i]
+    return result
