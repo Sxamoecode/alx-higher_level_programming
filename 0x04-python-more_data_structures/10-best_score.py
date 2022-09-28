4e4282fd38edb217d93f57d0c4ca922c6b16ecc7
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-28 12:29:07 -0700

    Updating

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    key_max = max(a_dictionary, key= lambda x: a_dictionary[x])
+    return key_max
