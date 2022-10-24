0c8312ad053cd78a84d5b9f603ac48cdc76572c5
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-24 11:44:12 -0700

    Updating

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+check for inheritance
+"""
+
 
 def inherits_from(obj, a_class):
+    """function that returns True
+    if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+
+    :param obj: class to be compared
+    :param a_class: class to compare with
+    :return: True
+    """
     if type(obj) == a_class or isinstance(obj, a_class):
         return True
     return False
