d1014066a6d2917a029b1df9d03b8c3ebbf4d814
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-24 11:02:53 -0700

    Updating

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Check if for similar classes
+"""
+
+
 def is_same_class(obj, a_class):
+    """
+    function that returns true if classes are same
+    :param obj: class to be compared
+    :param a_class: class to compare with
+    :return: True
+    """
     if isinstance(obj, a_class):
         return True
