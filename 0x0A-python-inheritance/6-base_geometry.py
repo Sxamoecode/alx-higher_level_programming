1664f00a86a7816e7470c0f8bbfdcc6075e057ea
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-24 11:55:54 -0700

    Updating

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Basic geometry class
+"""
+
+
+class BaseGeometry:
+
+    """Creates a function area
+    """
+
+    def area(self):
+        """Raise exception
+        """
+        raise Exception("area() is not implemented")
