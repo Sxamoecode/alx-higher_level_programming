792c0856257f208f76f10b64133ba7c9886ae0d7
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-24 10:42:05 -0700

    Updating

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class MyList(list):
+    """
+    A class that inherits list
+    """
+
+    def print_sorted(self):
+        """
+        method to sort and print a list
+        :return: None
+        """
+        print(sorted(self))
