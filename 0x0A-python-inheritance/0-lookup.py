f0b73d546a1f0dcc762f66a3808115971d7db950
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-24 10:28:50 -0700

    Updating

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Lookup function
+"""
+
+
+def lookup(obj):
+    """
+    A function that returns the list of available attributes and methods of an object.
+    :param obj: the object to be returned
+    :return: list
+    """
+    return dir(obj)
+
