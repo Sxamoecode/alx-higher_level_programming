96db52f56b547b13716755e16229c1c464f3a79f
Ayandare Samuel <samuelayandare@gmail.com>
2022-10-25 12:27:01 -0700

    Updating

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """A function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, mode='r', encoding="UTF8") as readFile:
-        readFile.read()
+        print(readFile.read())
