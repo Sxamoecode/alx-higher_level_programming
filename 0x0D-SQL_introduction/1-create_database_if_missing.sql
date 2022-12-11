4bfea3869a67fd14a43a485bfd39fda3274260d7
Ayandare Samuel <samuelayandare@gmail.com>
2022-12-11 11:14:26 -0800

    Updating

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,5 @@
 -- Creates database if not exists
-mysql -u root -p
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+mysql -u root -p;
+CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+[CHARACTER SET charset_name];
+[COLLATE collation_name]
