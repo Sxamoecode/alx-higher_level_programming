1f1c00cc9dc46c5c60b9232c05379333cd701be3
Ayandare Samuel <samuelayandare@gmail.com>
2022-12-21 12:48:24 -0800

    Updating

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const str = 'C is fun';
 if (isNaN(Number(process.argv[2])) === true) {
-    console.log('Missing number of occurrences');
-    return;
-};
-for (let i = 0; i < process.argv[2]; i++) {
-  console.log(str);
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(str);
+  }
 }
