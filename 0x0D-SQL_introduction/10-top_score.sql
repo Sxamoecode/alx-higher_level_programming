560cdea5b2429c3b4c72f19f2075d3a988caa499
Sxamoecode <samuelayandare@gmail.com>
2023-09-22 11:29:23 +0100

    SQL intro

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score, i.e largest to lowest.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
