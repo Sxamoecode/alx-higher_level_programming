ce7a9ba4ac4f0c5369251d8b87998ca4afea4ae3
Sxamoecode <samuelayandare@gmail.com>
2023-09-22 13:15:59 +0100

    python network0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,19 +10,19 @@ def find_peak(list_of_integers):
         return None
 
     length = len(list_of_integers)
-    j = int(length / 2)
-    list_int = list_of_integers
+    m = int(length / 2)
+    li = list_of_integers
 
-    if j - 1 < 0 and j + 1 >= length:
-        return list_int[j]
-    elif j - 1 < 0:
-        return list_int[j] if list_int[j] > list_int[j + 1] else list_int[j + 1]
-    elif j + 1 >= length:
-        return list_int[j] if list_int[j] > list_int[j - 1] else list_int[j - 1]
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
 
-    if list_int[j - 1] < list_int[j] > list_int[j + 1]:
-        return list_int[j]
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
 
-    if list_int[j + 1] > list_int[j - 1]:
-        return find_peak(list_int[j:])
-    return find_peak(list_int[:j])
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
